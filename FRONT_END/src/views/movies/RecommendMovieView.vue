a8f149fde8e760894b612481842b428f16aea52f
HwanSeok Choi <choihs2393@gmail.com>
2020-06-13 04:12:06 +0900

    Signup, RecommendMovie, Mypage, MovieSearch 틀일

---
+++ FRONT_END/src/views/movies/RecommendMovieView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h3>304</h3>
+    <p>"어디에서, 어떻게, 누구와 보는가가 영화의 완성이다." -왕가위</p>
+    <select class="custom-select">
+      <option selected>랜덤영화</option>
+      <option value="1">로맨스</option>
+      <option value="2">범죄</option>
+      <option value="3">액션</option>
+    </select>
+    <img src="../../assets/logo.png" alt="">
+    <p>신과 함께: 인과 연</p>
+    <p>2017-한국</p>
+    <p>***</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RecommendMovieView',
+}
+</script>
+
+<style>
+
+</style>
