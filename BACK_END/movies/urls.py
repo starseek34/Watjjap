e2959f2c351aba0ee4ea5d7a8c2ee0ce630456ea
김상현 <smile_666@naver.com>
2020-06-14 16:51:02 +0900

    asdf

--- BACK_END/movies/urls.py
+++ BACK_END/movies/urls.py
@@ -7,12 +7,7 @@ app_name = 'movies'
 urlpatterns = [
     path('search/<str:inputValue>', views.search),
     path('<int:movie_pk>/',views.movie),
-<<<<<<< HEAD
-    path('<int:movie_pk>/delete/',views.movie_delete),
-    path('save_movie/', views.save_movie),
-=======
     path('<int:movie_pk>/delete/',views.movie_delete),   
     #영화저장
-    path('save_movie/', views.save_movie), 
->>>>>>> 1a837cbaf8dbfebd24af197aec39cb786310ed6e
+    path('save_movie/', views.save_movie),
 ]
