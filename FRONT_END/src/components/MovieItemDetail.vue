0f363ad53613501f620bb50ed99c0e021a815060
Starseeker <eight34@naver.com>
2020-06-15 22:17:55 +0900

    variable routing with search

--- FRONT_END/src/components/MovieItemDetail.vue
+++ FRONT_END/src/components/MovieItemDetail.vue
@@ -1,6 +1,11 @@
 <template>
-  <div>
-    {{ movie }}
+  <div class="card" style="width: 18rem;">
+    <img :src="poster" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">{{ movie.title }}</h5>
+      <h6 class="card-subtitle mb-2 text-muted">{{ movie.director }} {{ movie.pubDate }}</h6>
+      <p class="card-text">{{movie.userRating}}</p>
+    </div>
   </div>
 </template>
 
@@ -9,6 +14,11 @@ export default {
   name: 'MovieItemDetail',
   props: {
     movie: Object,
+  },
+  computed: {
+    poster(){
+      return this.movie.image
+    }
   }
 }
 </script>
