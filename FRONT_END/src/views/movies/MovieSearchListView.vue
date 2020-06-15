63009cfe2b13d564ed623003d2b3a1c5e6c4643b
SangEun <eight34@naver.com>
2020-06-15 17:18:27 +0900

    search function

--- FRONT_END/src/views/movies/MovieSearchListView.vue
+++ FRONT_END/src/views/movies/MovieSearchListView.vue
@@ -1,15 +1,8 @@
 <template>
   <div>
     <h2>검색 결과</h2>
-    <router-link :to="{ name: 'MovieDetail' }">
-      <div>
-        <img src="../../assets/logo.png" alt="영화포스터_검색결과">
-        <h4>기생충</h4>
-        <p>2019 - 한국</p>
-        <p>감독 - 봉준호</p>
-        <p>평점 - 4.5</p>
-      </div>
-    </router-link>
+    <SearchBar @input-change='search' />
+    <MovieItem />
   </div>
 </template>
 
