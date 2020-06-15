f223219259e387c621bc604ba2f2cbacb7e00098
Starseeker <eight34@naver.com>
2020-06-15 22:28:24 +0900

    s'

--- FRONT_END/src/components/MovieItemDetail.vue
+++ FRONT_END/src/components/MovieItemDetail.vue
@@ -2,9 +2,9 @@
   <div class="card" style="width: 18rem;">
     <img :src="poster" class="card-img-top" alt="...">
     <div class="card-body">
-      <h5 class="card-title">{{ movie.title }}</h5>
-      <h6 class="card-subtitle mb-2 text-muted">{{ movie.director }} {{ movie.pubDate }}</h6>
-      <p class="card-text">{{movie.userRating}}</p>
+      <h5 class="card-title">{{ newTitle }}</h5>
+      <h6 class="card-subtitle mb-2 text-muted">{{ newDirector }} {{ movie.pubDate }}</h6>
+      <p class="card-text"> Naver 평점 : {{movie.userRating}}</p>
     </div>
   </div>
 </template>
@@ -14,6 +14,11 @@ export default {
   name: 'MovieItemDetail',
   props: {
     movie: Object,
+  },
+  data(){
+    return{
+      newTitle : this.movie.title.replace(/(<([^>]+)>)/ig,""),
+    }
   },
   computed: {
     poster(){
