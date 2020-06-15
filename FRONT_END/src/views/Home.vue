fb0a1295dede15b51d8f1de5b292ee962d7f22dc
김상현 <smile_666@naver.com>
2020-06-15 14:38:43 +0900

    진짜됐따

--- FRONT_END/src/views/Home.vue
+++ FRONT_END/src/views/Home.vue
@@ -1,7 +1,9 @@
 <template>
+    <!-- Login Button trigger modal -->
+  
   <div id="home">
  
-<!-- Login Button trigger modal -->
+    <!-- Login Button trigger modal -->
     <!-- Login Modal -->
     <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="loginModalLabel" aria-hidden="true">
       <div class="modal-dialog">
