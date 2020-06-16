8e182e716431f43c1822fb34330147a451e745f2
SangEun <eight34@naver.com>
2020-06-16 22:10:08 +0900

    merging

--- FRONT_END/src/router/index.js
+++ FRONT_END/src/router/index.js
@@ -8,6 +8,7 @@ import Modal from '../components/Modal.vue'
 import RecommendMovieView from '../views/movies/RecommendMovieView.vue'
 import MovieSearchListView from '../views/movies/MovieSearchListView.vue'
 import MovieDetailView from '../views/movies/MovieDetailView.vue'
+import ReviewView from '../views/movies/ReviewView.vue'
 
 // accounts
 import MyPageView from '../views/accounts/MyPageView.vue'
@@ -50,6 +51,11 @@ Vue.use(VueRouter)
     name: 'MovieDetail',
     component: MovieDetailView,
   },
+  {
+    path: 'movies/detail/:movieId/review',
+    name: 'Review',
+    component: ReviewView,
+  }
 ]
 
 const router = new VueRouter({
