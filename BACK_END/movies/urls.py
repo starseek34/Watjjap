dc5447f7a1989ab655f17c84c18f10b5c9642b97
HwanSeok Choi <choihs2393@gmail.com>
2020-06-16 16:20:51 +0900

    complete SimilarMovie

--- BACK_END/movies/urls.py
+++ BACK_END/movies/urls.py
@@ -5,10 +5,10 @@ from . import views
 app_name = 'movies'
 
 urlpatterns = [
-    path('search/<str:inputValue>', views.search),
+    path('search/<str:inputValue>/', views.search),
     path('<int:movie_pk>/',views.movie),
     path('<int:movie_pk>/delete/',views.movie_delete),
     #영화저장
     path('save_movie/', views.save_movie), 
-    path('search_genre/<str:inputValue>', views.search_genre),
+    path('search_genre/<str:inputValue>/', views.search_genre),
 ]
