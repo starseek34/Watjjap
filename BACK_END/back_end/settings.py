84e65a026a25ba0e1b64ec6543cdab3cd9abd0d6
김상현 <smile_666@naver.com>
2020-06-18 07:56:50 +0900

    hide secret key

--- BACK_END/back_end/settings.py
+++ BACK_END/back_end/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,10 +21,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'y=($*c7c##hndl4cj#)-721l^&vvl5_b=pme_9s3%u#!a-!_*+'
+# SECRET_KEY = 'y=($*c7c##hndl4cj#)-721l^&vvl5_b=pme_9s3%u#!a-!_*+'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = []
 
